50f75caee2cd5be1abe13378021ef49c9fe1a194
thititongumpun <thiti180536@gmail.com>
2023-04-07 22:25:30 +0700

    feat(devtoapi): add response 10 perpages

--- src/libs/devtoApi.ts
+++ src/libs/devtoApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchData = async () => {
-  const { data } = await axios.get('https://dev.to/api/articles?page=1&per_page=5');
+  const { data } = await axios.get('https://dev.to/api/articles?page=1&per_page=10');
   
   return data;
 }
